f7e977ac99f4a2f10a09977a4b3d013af116426b
lihongwei1239@outlook.com <lihongwei1239@outlook.com>
2017-09-29 14:43:58 +0800

    dev

---
+++ spring-cloud-hystrix/spring-cloud-consumer-hystrix/src/main/java/com/springcloud/consumer/hystrix/springcloudconsumerhystrix/service/HelloRemote.java
@@ -0,0 +1,14 @@
+package com.springcloud.consumer.hystrix.springcloudconsumerhystrix.service;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "spring-cloud-producer",fallback=HelloRemoteHystrix.class)
+public interface HelloRemote {
+
+    @RequestMapping("/hello")
+    String hello(@RequestParam("name") String name);
+
+
+}
